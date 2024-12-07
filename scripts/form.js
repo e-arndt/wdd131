7a764a9105a12532d96d16bd69948878ba4f96f3
e-arndt <earndt1701@gmail.com>
2024-12-07 00:32:47 -0800

    work in progress

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,12 @@
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const currentDate = new Date();
+const yearSpan = document.querySelector('#currentYear');
+
+yearSpan.innerText = currentDate.getFullYear();
+
+
+
 const input = document.querySelector('#favchap');
 const addButton = document.querySelector('button');
 const list = document.querySelector('#list');
@@ -48,3 +57,32 @@ function deleteChapter(chapter) {
     chaptersArray = chaptersArray.filter((item) => item !== chapter);
     setChapterList();
 }
+
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
